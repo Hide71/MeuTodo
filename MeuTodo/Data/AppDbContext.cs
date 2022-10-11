869cc4a854b7ea1dcbe8e6dd6f1723c27dd973e7
Hideraldo Rodrigues Marciano <95453443+Hide71@users.noreply.github.com>
2022-10-11 15:09:25 -0300

    novo

---
+++ MeuTodo/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MeuTodo.Models;
+namespace MeuTodo.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Todo>Todos { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder OptionsBuilder)
+            => OptionsBuilder.UseSqlite(connectionString: "DataSource=app.db;Cache=Shared");
+    }
+}
