dd97cd7492d5aa82e8c2a85aeb50e8b50a481813
Hideraldo Rodrigues Marciano <95453443+Hide71@users.noreply.github.com>
2022-10-11 15:41:12 -0300

    novo2

---
+++ MeuTodo/Controllers/TodoController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using MeuTodo.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace MeuTodo.Controllers
+{
+    [ApiController]
+    [Route(template:"v1")]
+    public class TodoController : ControllerBase
+    {
+        [HttpGet]
+        [Route("todos")]
+        public List<Todo> Get()
+        {
+            return new List<Todo>();
+
+        }
+    }
+}
